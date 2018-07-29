d86cd98c2c65a342354d2b769ce99f65aaa066e8
wael20-meet <wael20@meet.mit.edu>
2018-07-29 11:12:07 -0400

    Stopping here

--- MEETinTurtle.py
+++ MEETinTurtle.py
@@ -1,4 +1,5 @@
 
+
 import turtle
 
 # Everything that comes after the # is a 
@@ -6,17 +7,6 @@ import turtle
 # It is a note to the person reading the code.
 # The computer ignores it.
 # Write your code below here...
-turtle.penup() 	#Brings the pen up, so
-				#nothing will be drawn
-turtle.pendown()	#Puts the pen down, so we
-				#are ready to draw!
-turtle.goto(x,y) #Go to the position “x"&"y", 
-#but write in numbers 
-#instead
-
-
-# ...and end it before the next line.
-turtle.mainloop()
 
 turtle.penup() #Pick up the pen so it doesn’t 
                #draw
@@ -30,5 +20,53 @@ turtle.goto(-200,-100+200)
 turtle.goto(-200+50,-100) 
 turtle.goto(-200+100,-100+200)
 turtle.goto(-200+100,-100) 
+
+
+turtle.penup() #Pick up the pen so it doesn’t 
+               #draw
+turtle.goto(-200,-100) #Move the turtle to the 
+ #position (-200, -100) 
+ #on the screen
+turtle.pendown() #Put the pen down to start
+                 #drawing
+#Draw the M:
+turtle.goto(-200,-100+200) 
+turtle.goto(-200+50,-100) 
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+turtle.penup()
+turtle.goto(-50,-100)
+
+turtle.pendown()
+turtle.goto(-50,+100)
+turtle.goto(+50,+100)
+turtle.goto(-50,+100)
+turtle.goto(-50,-15)
+turtle.goto(50,-15)
+turtle.goto(-50,-15)
+turtle.goto(-50,-100)
+turtle.goto(+50,-100)
+turtle.penup()
+turtle.goto(100,-100)
+turtle.pendown()
+turtle.goto(100,+100)
+turtle.goto(200,100)
+turtle.goto(100,+100)
+turtle.goto(100,-10)
+turtle.goto(200,-10)
+turtle.goto(100,-10)
+turtle.goto(100,-100)
+turtle.goto(200,-100)
+turtle.penup()
+turtle.goto(300,-100)
+turtle.pendown()
+turtle.goto(300,100)
+turtle.goto(225,100)
+turtle.goto(375,100)
+# ...and end it before the next line.
+turtle.mainloop()
+
+
 
 
